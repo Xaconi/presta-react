4ee5cda064b4b70b890698e0d3bf4edcdc070163
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2020-08-09 18:10:55 +0200

    [NEW] Event-driven for cart updates.

--- components/Cart.js
+++ components/Cart.js
@@ -1,7 +1,22 @@
-export default function Cart() {
+import { prestaEvents } from '../lib/presta-api';
+import { useState } from 'react';
+
+export default function Cart({cart}) {
+
+    function setCart(cart) {
+    }
+
+    [cart, setCart] = useState(0);
+
+    prestaEvents.on('updateCart', function(cart){
+        console.log(cart);
+        setCart(cart);
+    })
+
     return (
         <>
             <p>Cart</p>
+            <p>You have {cart?.associations?.cart_rows.length} items on your cart.</p>
         </>
     )
 }
@@ -10,6 +25,8 @@ export async function getStaticProps() {
     console.log("CLIENT CART");
 
     return {
-        props : {}
+        props : {
+            cart : null
+        }
     }
 }
